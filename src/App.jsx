2b663f181c7841eb645a70578bfef8d3f4edfcbf
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-28 12:14:18 +0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import Form from "./Form";
+import Values from "values.js";
+import ColorList from "./ColorList";
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values("#ddd").all(10));
+
+  const addColor = (color) => {
+    console.log(color);
+    try {
+      const newColor = new Values(color).all(10);
+      setColors(newColor);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />;
+    </main>
+  );
 };
 export default App;
